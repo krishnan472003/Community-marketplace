1b6352f42cedaa63bfff5f6a6b99355570ac1c27
krishnan472003 <99252508+krishnan472003@users.noreply.github.com>
2023-10-29 19:52:10 +0530

    razorpay

--- backend/User/user.router.ts
+++ backend/User/user.router.ts
@@ -2,19 +2,18 @@ import { Router } from "express"
 import { addCart } from "./Router/addCart";
 import  { change }  from "./Router/changeProfile";
 import { getUserProfile } from "./Router/getProfile";
-
-
-
 import { updateCart } from "./Router/updateCart";
+import { getCart } from "./Router/getCart";
 
 
 
 export const UserModule = ()=>{
     const router = Router();
+    
         router.use("/user",addCart());
         router.use("/user",change());
         router.use("/user",getUserProfile());
-        
+        router.use("/user",getCart());
         router.use("/user",updateCart());
     return router
 }
--- src/Screens/BuyProduct/BuyProduct.jsx
+++ src/Screens/BuyProduct/BuyProduct.jsx
@@ -1,34 +1,40 @@
 import * as React from 'react';
-
+import axios from 'axios';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Navbar from "../../components/Navbar"
 import Footer from "../../components/Footer"
-import Buttons from "../../components/Button"
+import Button from "../../components/Button"
 import { useLocation } from 'react-router';
-
-// const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: 'center',
-//   color: theme.palette.text.secondary,
-// }));
-
+import { useNavigate } from 'react-router-dom';
 
 function BuyProduct() {
+  const navigate = useNavigate()
   const location = useLocation();
   const product = location.state
   const handleAddToCart = () => {
-
+    console.log("=======")
       const data = {
-        token :localStorage.get("token"),
-        name:product.name,
+        token :localStorage.getItem("token"),
+        uId : localStorage.getItem('uId'),
         quantity:1,
         pId : product.pId,
         
       }
-      axios.post("http://localhost:5000/api/user/addCart")
+      axios.post("http://localhost:5000/api/user/addCart",data)
+      .then((data)=>{
+          if(data.status == 200){
+            navigate('/cart')
+          }
+          else{
+            alert(data)
+          }
+      })
+      .catch((err)=>{
+        console.log(err)
+      }
+      
+      )
   }
   
   return (
@@ -44,11 +50,13 @@ function BuyProduct() {
   <h4>Description:</h4>
    <p>{product.description}</p>
 
-    <Buttons
+    <Button
         variant="contained"
         text = "Add to Cart"
         onClick={handleAddToCart}
     />
+            <button onClick={handleAddToCart}>ad</button>
+
     </Grid>
 </Grid>
 <Footer/>
