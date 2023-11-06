1aaa312f7a9bb03973ed8eb58de812176d835910
soniyanandwani7 <soniyanandwani7@gmail.com>
2023-11-06 22:45:15 +0530

    modified finance page

--- src/Screens/Fianance/Fianance.jsx
+++ src/Screens/Fianance/Fianance.jsx
@@ -43,7 +43,8 @@ function Fianance()
 
     return(<div>
      <Navbar />
-     <Grid container spacing={2}  marginLeft={45} marginTop={10}>
+     
+     {/* <Grid container spacing={2}  marginLeft={45} marginTop={10}>
      
      <Grid item xs={6}>
      <DemoPaper variant="elevation">
@@ -83,18 +84,27 @@ function Fianance()
         </Grid>
 
      </DemoPaper>
-     </Grid>
+     </Grid> */}
   
     
 
 
+     {/* </Grid> */}
+     <Grid container direction={'column'} justifyContent={'center'} alignItems={'center'} marginTop={5}>
+      <Grid item xs>
+      <h4>Your Order has been successfully placed!!</h4>
+      </Grid>
+      <Grid item xs>
+        <a href="/" style={{color:'#04AA6D'}}>Continue Shopping</a>
+      </Grid>
      </Grid>
-     <Grid container spacing={2} marginLeft={8}   marginTop={4} >
-     <h2 style={textStyle}  >Your Past Orders:</h2>
+     
+     <Grid container spacing={2} marginLeft={3}   marginTop={4} >
+     <h2 style={textStyle}  >Your Past Orders</h2>
      </Grid>
-     <Box sx={{ flexGrow: 1 }} margin={5}>
+     <Box sx={{ flexGrow: 1 }} margin={3}>
       <Grid container spacing={3} marginBottom={2}>
-        <Grid item xs={5}>
+        <Grid item xs={6}>
         <h6>Product</h6>
         </Grid>
         <Grid item xs>
@@ -102,9 +112,6 @@ function Fianance()
         </Grid>
         <Grid item xs>
         <h6>Quantity</h6>
-        </Grid>
-        <Grid item xs>
-        <h6>Total</h6>
         </Grid>
       </Grid>
 
@@ -118,19 +125,23 @@ function Fianance()
         </Grid>
         <Grid item xs direction="column">
           <h4>Sofa</h4>
-          <div>Remove</div>
+          <div>
+            <h6>ProductId:</h6>
+            <h6>OrderId:</h6>
+          </div>
         </Grid>
         <Grid item xs>
           <h6>$9.99</h6>
         </Grid>
         <Grid item xs>
-        <select name="quantity" id="">
+        {/* <select name="quantity" id="">
           <option value="1">1</option>
-        </select>
+        </select> */}
+          <h6>1</h6>
         </Grid>
-        <Grid item xs>
+        {/* <Grid item xs>
         <h6>$10.5</h6>
-        </Grid>
+        </Grid> */}
       </Grid>
 
       <Grid>
